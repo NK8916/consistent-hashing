129c7f4b230e779b4e4069548fea29fe5d9a1659
Nitin Kumar <2018kumarnitin@gmail.com>
2025-08-02 03:50:44 +0530

    added sha-1 and mummur3 algo

--- src/main/java/io/github/NK8916/hashImplementations/Murmur3HashFunction.java
+++ src/main/java/io/github/NK8916/hashImplementations/Murmur3HashFunction.java
@@ -1,4 +1,16 @@
 package io.github.NK8916.hashImplementations;
 
-public class Murmur3HashFunction {
+import com.google.common.hash.Hashing;
+
+import io.github.NK8916.HashFunction;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+
+public class Murmur3HashFunction implements HashFunction{
+    public BigInteger hash(String key){
+        int hash=Hashing.murmur3_32_fixed().hashString(key, StandardCharsets.UTF_8).asInt();
+        long unsigned=hash & 0xffffffffL;
+        return BigInteger.valueOf(unsigned);
+    }
 }
