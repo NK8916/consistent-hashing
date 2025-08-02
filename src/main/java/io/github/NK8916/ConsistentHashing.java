79b72e06001e9dbd75073ec84697ebc169531ce6
Nitin Kumar <2018kumarnitin@gmail.com>
2025-08-03 02:55:46 +0530

    add crud operarions

--- src/main/java/io/github/NK8916/ConsistentHashing.java
+++ src/main/java/io/github/NK8916/ConsistentHashing.java
@@ -1,6 +1,7 @@
 package io.github.NK8916;
 
 import java.math.BigInteger;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class ConsistentHashing {
@@ -9,6 +10,24 @@ public class ConsistentHashing {
 
     ConsistentHashing(HashFunction hashFunction){
         this.hashFunction=hashFunction;
+    }
 
+    public void add(String node){
+        BigInteger nodeHash=this.hashFunction.hash(node);
+        ring.put(nodeHash,node);
+    }
+
+    public void remove(String node){
+        BigInteger nodeHash=this.hashFunction.hash(node);
+        ring.remove(nodeHash);
+    }
+
+    public String getKey(String node){
+        BigInteger keyHash=this.hashFunction.hash(node);
+        Map.Entry<BigInteger,String> entry=ring.ceilingEntry(keyHash);
+        if(entry==null){
+            entry=ring.firstEntry();
+        }
+        return entry.getValue();
     }
 }
--- src/main/java/io/github/NK8916/Main.java
+++ src/main/java/io/github/NK8916/Main.java
@@ -1,7 +1,11 @@
 package io.github.NK8916;
 
+import io.github.NK8916.hashImplementations.Murmur3HashFunction;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Consistent Hashing PoC");
+        HashFunction hashFunction=new Murmur3HashFunction();
+        ConsistentHashing ring =new ConsistentHashingBuilder().withHashFunction(hashFunction).build();
+
     }
 }
